54d8ed016eca81e82c9b2e8f61dfce3ff3bd35e6
BlasParedes <blas.12@hotmail.es>
2017-04-06 08:02:47 -0400

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -8,17 +8,17 @@
  
  Note that this sketch uses LED_BUILTIN to find the pin with the internal LED
 */
-
+#define LED 02
 void setup() {
-  pinMode(02, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  pinMode(LED, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(02, LOW);   // Turn the LED on (Note that LOW is the voltage level
+  digitalWrite(LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
                                     // but actually the LED is on; this is because 
                                     // it is acive low on the ESP-01)
   delay(1000);                      // Wait for a second
-  digitalWrite(02, HIGH);  // Turn the LED off by making the voltage HIGH
+  digitalWrite(LED, HIGH);  // Turn the LED off by making the voltage HIGH
   delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
